b0306a47af8a14f30c2c4f45c569070563bfc95e
Thuc Le <ledongthuc@gmail.com>
2019-12-03 12:59:47 +0100

    draft commit with ingress nginx

---
+++ k8s/jobs/service_with_ingress_nginx_custom_authenticate/main.go
@@ -0,0 +1,51 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	CookieAuthenticate = "AUTHENTICATED"
+)
+
+func main() {
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:  CookieAuthenticate,
+			Value: "true",
+		})
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   CookieAuthenticate,
+			Value:  "false",
+			MaxAge: 0,
+		})
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.HandleFunc("/authenticate", func(w http.ResponseWriter, r *http.Request) {
+		c, err := r.Cookie(CookieAuthenticate)
+		if err != nil {
+			fmt.Fprintf(w, err.Error())
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		if c == nil || c.Value == "true" {
+			fmt.Fprintf(w, "Forbidden")
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
