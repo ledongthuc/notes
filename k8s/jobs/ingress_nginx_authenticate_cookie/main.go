fe358cc61b3ca71d1ef80e7c80c1d3e2fd1c8059
Thuc Le <ledongthuc@gmail.com>
2020-01-29 05:23:01 +0100

    Update snippet

--- k8s/jobs/ingress_nginx_authenticate_cookie/main.go
+++ k8s/jobs/ingress_nginx_authenticate_cookie/main.go
@@ -12,11 +12,18 @@ const (
 
 func main() {
 	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		user := r.URL.Query()["user"]
+		if len(user) == 0 || user[0] != "password" {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Login fail"))
+			return
+		}
 		http.SetCookie(w, &http.Cookie{
 			Name:  CookieAuthenticate,
 			Value: "true",
 		})
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Login success"))
 	})
 
 	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
@@ -26,24 +33,27 @@ func main() {
 			MaxAge: 0,
 		})
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Logout success"))
 	})
 
 	http.HandleFunc("/authenticate", func(w http.ResponseWriter, r *http.Request) {
 		c, err := r.Cookie(CookieAuthenticate)
 		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
 			fmt.Fprintf(w, err.Error())
-			w.WriteHeader(http.StatusForbidden)
 			return
 		}
 
-		if c == nil || c.Value == "true" {
+		if c == nil || c.Value == "false" {
+			w.WriteHeader(http.StatusUnauthorized)
 			fmt.Fprintf(w, "Forbidden")
-			w.WriteHeader(http.StatusForbidden)
 			return
 		}
 
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Authenticated"))
 	})
 
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	fmt.Println("Start server")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
