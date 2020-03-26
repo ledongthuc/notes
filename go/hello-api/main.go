bb3fa7f6534face3712c082769c232cae1a373b7
Thuc Le <ledongthuc@gmail.com>
2020-03-26 08:17:57 +0100

    Update example about production

--- go/hello-api/main.go
+++ go/hello-api/main.go
@@ -12,15 +12,21 @@ func main() {
 	e := echo.New()
 	customRoutes := getRoutes()
 
+	customRoot := false
 	for index := range customRoutes {
 		route := customRoutes[index]
+		if route.path == "/" || route.path == "" {
+			customRoot = true
+		}
 		e.GET(route.path, func(c echo.Context) error {
 			return c.String(http.StatusOK, route.message)
 		})
 	}
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	if !customRoot {
+		e.GET("/", func(c echo.Context) error {
+			return c.String(http.StatusOK, "Hello, World!")
+		})
+	}
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
