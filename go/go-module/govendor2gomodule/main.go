accf283c492a1d79eb0a8ad87dfd238dca4c5299
Thuc Le <ledongthuc@gmail.com>
2020-04-05 20:33:52 +0200

    Update go module

--- go/go-module/govendor2gomodule/main.go
+++ go/go-module/govendor2gomodule/main.go
@@ -4,18 +4,12 @@ import (
 	"fmt"
 
 	"rsc.io/quote"
-	quoteV3 "rsc.io/quote/v3"
 )
 
 func main() {
 	fmt.Println("Hello world: ", getQuote())
-	fmt.Println("Hello world 3: ", getQuoteV3())
 }
 
 func getQuote() string {
 	return quote.Go()
-}
-
-func getQuoteV3() string {
-	return quoteV3.Concurrency()
 }
