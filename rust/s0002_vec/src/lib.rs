92b61e85bac5eb3c920b11b7dbc40f3e62affbdc
Thuc Le <ledongthuc@gmail.com>
2022-10-19 23:51:15 +0200

    Rust: Test Vec::from_raw_parts

--- rust/s0002_vec/src/lib.rs
+++ rust/s0002_vec/src/lib.rs
@@ -1,8 +1,11 @@
 #[cfg(test)]
 mod tests {
+    use core::ptr;
+    use std::alloc::{alloc, Layout};
+
     #[test]
     fn basic_vec() {
-        let mut vec = vec![1,2,3];
+        let mut vec = vec![1, 2, 3];
         assert_eq!(3, vec.len());
         let old_cap = vec.capacity();
         assert_eq!(3, vec.capacity());
@@ -30,5 +33,26 @@ mod tests {
 
         let vec: Vec<()> = Vec::with_capacity(100);
         assert_eq!(usize::MAX, vec.capacity());
+    }
+
+    #[test]
+    fn vec_from_raw_parts() {
+        let layout = Layout::array::<u16>(5);
+        let ptr = unsafe { alloc(layout.unwrap()) };
+        assert!(!ptr.is_null());
+        let ptr = ptr as *mut u16;
+        unsafe { ptr::write(ptr, 15) };
+        unsafe { ptr::write(ptr.add(1), 14) };
+        unsafe { ptr::write(ptr.add(2), 13) };
+        unsafe { ptr::write(ptr.add(3), 12) };
+        unsafe { ptr::write(ptr.add(4), 11) };
+        unsafe { ptr::write(ptr.add(5), 10) };
+
+        let vec = unsafe { Vec::from_raw_parts(ptr, 5, 5) };
+        assert_eq!(vec[0], 15);
+        assert_eq!(vec[1], 14);
+        assert_eq!(vec[2], 13);
+        assert_eq!(vec[3], 12);
+        assert_eq!(vec[4], 11);
     }
 }
