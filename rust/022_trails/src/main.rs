3290fa3603ddcc4acc0a31ccd55b8ec1290e0626
Thuc Le <ledongthuc@gmail.com>
2022-10-19 23:12:36 +0200

    Test shared trait

--- rust/022_trails/src/main.rs
+++ rust/022_trails/src/main.rs
@@ -43,3 +43,40 @@ fn main() {
 
     Tweet::notify(&tweet);
 }
+
+pub trait SharedTrait {
+    fn need_to_implement(&self) -> &str;
+    fn default_implementation(&self) -> &str {
+        "Method is implemented in traits"
+    }
+}
+
+pub struct Struct1 {}
+impl SharedTrait for Struct1 {
+     fn need_to_implement(&self) -> &str {
+        "This method is implemented by struct 1"
+    }
+}
+
+pub struct Struct2 {}
+impl SharedTrait for Struct2 {
+     fn need_to_implement(&self) -> &str {
+        "This method is implemented by struct 2"
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Struct1, Struct2, SharedTrait};
+
+    #[test]
+    fn share_traits_methods() {
+        let s1 = Struct1{};
+        // assert_eq!("Method is implemented in traits", s1.default_implementation());
+        assert_eq!("This method is implemented by struct 1", s1.need_to_implement());
+
+        let s2 = Struct2{};
+        // assert_eq!("Method is implemented in traits", s2.default_implementation());
+        assert_eq!("This method is implemented by struct 2", s2.need_to_implement());
+    }
+}
