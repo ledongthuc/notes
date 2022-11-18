5a50d4411bcde80f898efae1acce62ec4f1cf470
Thuc Le <ledongthuc@gmail.com>
2022-11-18 17:22:41 +0100

    fix rust pattern

--- rust/025_pattern/src/lib.rs
+++ rust/025_pattern/src/lib.rs
@@ -7,7 +7,7 @@ mod tests {
     fn binding_subpattern_with_name() {
         let i = 10;
         let result = match i {
-            val @ 0 .. 11 => val * 10,
+            val @ 0..11 => val * 10,
             _ => -1,
         };
         assert_eq!(100, result);
@@ -17,25 +17,27 @@ mod tests {
     fn borrow() {
         let i = 10;
         let result = match i {
-            0 .. 10 => -1,
+            0..10 => -1,
             ref val => {
                 // val += 10; // can't += on &{integer}
                 val * 10
-            },
+            }
         };
         assert_eq!(100, result);
     }
 
+
+        enum Message {
+            First { id: i32 },
+        }
+
     #[test]
     fn string() {
-        let i = String::from("Hello");
+        let i = Message::First { id: 5 };
         let result = match i {
-            v @ String("Bye") => -1,
-            ref val => {
-                // val += 10; // can't += on &{integer}
-                val * 10
-            },
+            Message::First { id: id_variable @ 4..=6 } => id_variable,
+            Message::First { .. } => -1,
         };
-        assert_eq!(100, result);
+        assert_eq!(5, result);
     }
 }
