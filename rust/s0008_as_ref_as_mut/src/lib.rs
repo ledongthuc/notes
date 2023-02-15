e81f02012d2e9016d4dbab4f20bdd7120f6b9a47
Thuc Le <ledongthuc@gmail.com>
2023-02-15 21:47:00 +0100

    Rust: test AsRef, AsMut

---
+++ rust/s0008_as_ref_as_mut/src/lib.rs
@@ -0,0 +1,32 @@
+struct MyString {}
+
+impl AsRef<str> for MyString {
+    fn as_ref(&self) -> &'static str {
+        "my hello"
+    }
+}
+
+fn get_my_string<'a, T>(t: &'a T) -> &'a str where T : AsRef<str> {
+    t.as_ref()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_string() {
+        let s = "Hello".to_string();
+        let b = s.as_ref();
+
+        let expected : [u8; 5] = [72, 101, 108, 108, 111];
+        assert_eq!(b, expected);
+    }
+
+    #[test]
+    fn test_my_string() {
+        let my_s = MyString{};
+        let s = get_my_string(&my_s);
+        assert_eq!(s, "my hello")
+    }
+}
