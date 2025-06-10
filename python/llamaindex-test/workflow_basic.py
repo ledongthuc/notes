e686ceb0ce5281d7d6956055d7f1adc9b504f244
Thuc Le <ledongthuc@gmail.com>
2025-06-10 20:36:58 +0700

    Update llamaindex workflow with condition and loop

--- python/llamaindex-test/workflow_basic.py
+++ python/llamaindex-test/workflow_basic.py
@@ -11,6 +11,7 @@ from llama_index.utils.workflow import draw_all_possible_flows
 
 class FirstEvent(Event):
     first_output: str
+    count: int
 
 
 class SecondEvent(Event):
@@ -21,11 +22,13 @@ class MyWorkflow(Workflow):
     @step
     async def step_one(self, ev: StartEvent) -> FirstEvent:
         print(ev.first_input)
-        return FirstEvent(first_output="First step complete.")
+        return FirstEvent(first_output="First step complete.", count=1)
 
     @step
-    async def step_two(self, ev: FirstEvent) -> SecondEvent:
-        print(ev.first_output)
+    async def step_two(self, ev: FirstEvent) -> SecondEvent | FirstEvent:
+        print(ev.first_output, "Count: ", ev.count)
+        if ev.count < 5:
+            return FirstEvent(first_output="First step complete.", count=ev.count + 1)
         return SecondEvent(second_output="Second step complete.")
 
     @step
